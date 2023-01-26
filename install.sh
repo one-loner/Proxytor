84e69c11139087a70cab48036f00d116c982c5e2
one-loner <one-loner@tutanota.com>
2023-01-26 04:20:36 +0300

    Add torproxy to all commands adn fix install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+  then echo "Please run as root."
   exit
 fi
-apt-get install -y proxychains tor obfs4proxy
+apt-get install -y proxychains tor obfs4proxy privoxy
 cp proxytor.sh /bin/proxytor
 chmod +x /bin/proxytor
+cp torproxyon.sh /bin/torproxyon
+chmod +x /bin/torproxyon
+mv /etc/privoxy/config /etc/privoxy/config.old
+cp privoxy.config /etc/privoxy/config
+systemctl restart privoxy
+echo Proxytor is installed.
+echo proxytor command - to proxy command to Tor.
+echo torproxyon - to proxy all commands in this terminal session to Tor.
---
+++ torproxyon.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export http_proxy=http://127.0.0.1:8118
+export https_proxy=https://127.0.0.1:8118
+echo Tor proxy for commands is on.
+echo Enter exit to off Tor proxy.
+/bin/bash
